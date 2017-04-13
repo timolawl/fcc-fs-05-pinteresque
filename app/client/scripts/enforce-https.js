fb5efc965273c2164270d57d790799bd9a3bd889
timolawl <timolawl@gmail.com>
2017-04-13 09:34:50 -0700

    Correct domain name for enforcing https

--- app/client/scripts/enforce-https.js
+++ app/client/scripts/enforce-https.js
@@ -1,5 +1,5 @@
 export default () => {
-    const host = 'timolawl-imgesque.herokuapp.com';
+    const host = 'timolawl-pinteresque.herokuapp.com';
     if ((host == location.host) && (location.protocol != 'https:'))
         location.protocol = 'https';
 }
